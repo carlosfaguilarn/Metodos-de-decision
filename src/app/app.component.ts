f6e8e8264629e45f28c91b657aa864f0bfa4b9da
Carlos Aguilar <carlosf.aguilarn@gmail.com>
2018-09-01 20:35:29 -0600

    codigo con documentacion interna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,42 +9,45 @@ import { DOCUMENT } from '@angular/common';
 })
  
 export class AppComponent implements DoCheck {
-	title = 'metodos';
-	public columnas : number = 3; 
-	public filas : number = 3;
-	public col : number[] = [1];
-	public fil : number[] = [1];
-	public Datos : any;
-	public I : number;
-	public J : number;
+	title = 'metodos de decisión';
+	public columnas : number = 3;  //propiedad capturada en input
+	public filas : number = 3;     //propiedad capturada en input
+	public col : number[] = [1];   //Arreglo para iterar con la directiva ngFor
+	public fil : number[] = [1];   //Arreglo para iterar con la directiva ngFor
 
-	public datos  = [];
-
-	element: HTMLInputElement; 
+	public datos  = [];            //Objeto con los datos resultantes de la tabla
+	element: HTMLInputElement;     //elemento HTML para recoger los datos de las celdas de la tabla
 	
+	constructor(){}
 
-	constructor(){
-
-	}
 	ngDoCheck(){ 
+		//Actualiza el la longitud de la tabla (DataBinding para el DOM)
 		this.col.length = this.columnas;
 		this.fil.length = this.filas;	
 	}
 	onSubmit(){
+		//Imprimir longitud de la tabla
 		console.log("filas: "+this.fil.length);
 		console.log("columnas: "+this.col.length);
+
+		//Recorre las filas
 		for(var i = 0; i < this.fil.length; i++){
+			//Arreglo temporal para almacenar la fila[i]
 			var objeto = [];
+			//Recorre todas las columnas
 			for(var j = 0; j < this.col.length; j++){
+				//Busca y recoge el valor de la celda indexada y lo imprime
 				let id = "dato"+i+"_"+j;
 				this.element = document.getElementById(id) as HTMLInputElement; 
 				console.log(id+": "+this.element.value);
+
+				//'pushea' el valor al arreglo temporal para la fila
 				objeto.push(this.element.value);
-				//console.log(id);
 			}
+			//'pushea' la fila[i] en el objeto final
 			this.datos.push(objeto);
 		}
+		//Imprime el objeto final en consola
 		console.log(this.datos);
 	}
-
 }
