18560f7dc765555d5f18e8d5958c8a4c506008bd
Carlos Aguilar <carlosf.aguilarn@gmail.com>
2018-09-02 12:38:47 -0600

    funciona el maximin, imprime en consola el resultado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,8 @@ export class AppComponent implements DoCheck {
 
 	public datos  = [];            //Objeto con los datos resultantes de la tabla
 	element: HTMLInputElement;     //Elemento HTML para recoger los datos de las celdas de la tabla
+
+	public seleccion_maximin : number = 0;  //Resultado del método maximin
 	
 	constructor(){}
 
@@ -48,6 +50,42 @@ export class AppComponent implements DoCheck {
 			this.datos.push(objeto);
 		}
 		//Imprime el objeto final en consola
-		console.log(this.datos);
+		//console.log(this.datos);
+
+		let valor_maximin :string = this.maximin().valor;
+		let index_maximin : number = this.maximin().index + 1;
+		console.log("El valor Maximin es: "+valor_maximin+ " En la fila: "+index_maximin); 
+	}
+
+	maximin(){
+		var valores_minimos : number[] = [];	//Para guardar los minimos de cada fila
+
+		this.datos.forEach(function(valor, indice, array){
+			var numeros : number[] = [];	   //Arreglo temporal para numeros de la fila
+			valor.forEach(function(item, i, arreglo){
+				numeros.push(parseInt(item));	//Convierte a entero cada numero de la fila iterada
+			});
+
+			//Calcula el valor minimo de los numeros de la fila iterada y el indice
+			let minimo = Math.min.apply(null, numeros);
+
+			//Guarda el valor minimo de la fila en un arreglo general
+			valores_minimos.push(minimo);
+		});
+
+		//Calcula y retorna el valor máximo de los valores mínimos
+		let maximo_minimos, index_maximo;
+
+		valores_minimos.forEach((valor, i, array)=>{
+			if(valor === Math.max.apply(null, valores_minimos)){
+				maximo_minimos = valor;
+				index_maximo = i;
+			}
+		});
+
+		return {
+			"index" : index_maximo, 
+			"valor": maximo_minimos
+		};
 	}
 }
